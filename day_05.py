dc76184aa36fc6bfb7ae48303da7103e8e399bcc
ray <ray.voelker@gmail.com>
2019-12-05 11:16:30 -0500

    i think this works .. maybe

--- day_05.py
+++ day_05.py
@@ -1,7 +1,7 @@
 # day 05
 from array import array
 
-def final_state(input_intcode, is_slice=False):
+def final_state(input_intcode, is_slice=False, parameter_mode=False):
     """
     # doctests
     >>> final_state(array('l', [1,9,10,3,2,3,11,0,99,30,40,50]))
@@ -16,28 +16,60 @@ def final_state(input_intcode, is_slice=False):
     >>> final_state(array('l', [1,1,1,4,99,5,6,0,99]))
     array('l', [30, 1, 1, 4, 2, 5, 6, 0, 99])
 
-    # >>> final_state(array('l', [3,0,4,0,99]))
-    array('l', [0, 0, 4, 0, 99])
+    >>> final_state(array('l', [1002,4,3,4,33]), is_slice=False, parameter_mode=True)
+    array('l', [1002, 4, 3, 4, 99])
+
     """
     # for i in range(0, len(input_intcode)-4, 4):
     i = 0
     # set the number of intcode to jump based ont he op_code
     jump = 0
     while i < (len(input_intcode) - jump):
-        op_code = input_intcode[i]
+        if parameter_mode:
+            # position mode: 0
+            # immediate mode: 1
+
+            # Parameters that an instruction writes to will never be in
+            # immediate mode. (so, op codes 3, 4?)
+            string = str(input_intcode[i])
+            op_code = int(string[-2:])
+            mode_input_1 = int(string[-3:-2])
+            mode_input_2 = int(string[-4:-3])
+            # mode_input_3 = 0 # should be unused
+        else:
+            op_code = input_intcode[i]
+            mode_input_1 = 0
+            mode_input_2 = 0
+
         input_one = None
         input_two = None
 
         if op_code == 1:
             # add
-            input_one = input_intcode[input_intcode[i+1]]
-            input_two = input_intcode[input_intcode[i+2]]
+            if mode_input_1 == 0: 
+                input_one = input_intcode[input_intcode[i+1]]
+            else:
+                input_one = input_intcode[i+1]
+
+            if mode_input_2 == 0:
+                input_two = input_intcode[input_intcode[i+2]]
+            else:
+                input_two = input_intcode[i+2]
+
             input_intcode[input_intcode[i+3]] = input_one + input_two
             jump = 4
         elif op_code == 2:
-            input_one = input_intcode[input_intcode[i+1]]
-            input_two = input_intcode[input_intcode[i+2]]
             # multiply
+            if mode_input_1 == 0:
+                input_one = input_intcode[input_intcode[i+1]]
+            else:
+                input_one = input_intcode[i+1]
+
+            if mode_input_2 == 0:
+                input_two = input_intcode[input_intcode[i+2]]
+            else:
+                input_two = input_intcode[i+2]
+            
             input_intcode[input_intcode[i+3]] = input_one * input_two
             jump = 4
         elif op_code == 3:
@@ -67,4 +99,8 @@ if __name__ == "__main__":
     import doctest 
     print(doctest.testmod())
 
-print(final_state(array('l', [3,0,4,0,99])))
+print( )
+
+filename = 'day_05_input.txt'
+intcode = array('l', [int(value) for value in open(filename).read().split(",")])
+
